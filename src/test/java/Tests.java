e10b3b6eb9ba780bb1ebaec76d331bdc4020dae0
Gabriel Wyss <gw1@gmx.ch>
2017-12-30 12:25:30 +0100

    add better comments

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,13 +1,30 @@
+
+/**
+ *   Copyright 2018 Pascal Ammon, Gabriel Wyss
+ * 
+ * 	 Implementation eines anonymen Mobility Pricing Systems auf Basis eines Gruppensignaturschemas
+ * 
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
 import static org.junit.Assert.*;
-
 import java.util.Arrays;
-
 import org.junit.Assert;
 import org.junit.Test;
-
 import interfaces.HashValue;
 import util.HashHelper;
-
 
 public class Tests {
 
@@ -31,16 +48,15 @@ public class Tests {
 		public void setAnimal2(String animal2) {
 			this.animal2 = animal2;
 		}
-		
-		
-		
+
 	}
+
 	@Test
 	public void testHash() {
 		Demo d = new Demo();
 		d.setAnimal("dog");
 		d.setAnimal2("horse");
-		//assertEquals("dog".getBytes(), "dog".getBytes());
+		// assertEquals("dog".getBytes(), "dog".getBytes());
 		assertTrue(Arrays.equals("dog".getBytes(), HashHelper.getHash(d)));
 	}
 }
